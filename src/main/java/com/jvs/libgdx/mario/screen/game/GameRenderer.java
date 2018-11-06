6f30984011daf157aa6b9c33d589e32e7f9c9575
Jorge Vazquez <jorgevs.mx@gmail.com>
2018-11-05 22:28:12 -0500

    Adding Box2D library and grouping tiled objects

--- src/main/java/com/jvs/libgdx/mario/screen/game/GameRenderer.java
+++ src/main/java/com/jvs/libgdx/mario/screen/game/GameRenderer.java
@@ -5,9 +5,14 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.Logger;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -31,9 +36,15 @@ public class GameRenderer implements Disposable {
     private Viewport viewport;
     private ShapeRenderer shapeRenderer;
 
+    // Tiled map variables
     private TmxMapLoader tmxMapLoader;
     private TiledMap tiledMap;
     private OrthogonalTiledMapRenderer orthogonalTiledMapRenderer;
+
+    // Box2D variables
+    private World world;
+    private Box2DDebugRenderer box2DDebugRenderer;
+
 
     private TextureRegion playerRegion;
     private TextureRegion backgroundRegion;
@@ -65,6 +76,99 @@ public class GameRenderer implements Disposable {
         tiledMap = tmxMapLoader.load(AssetPaths.MarioTileMap);
         orthogonalTiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 
+        world = new World(new Vector2(0,0), true);
+        box2DDebugRenderer = new Box2DDebugRenderer();
+
+        BodyDef bodyDef = new BodyDef();
+        PolygonShape polygonShape = new PolygonShape();
+        FixtureDef fixtureDef = new FixtureDef();
+        Body body;
+
+        // Create ground bodies/fixtures
+        for(MapObject mapObject : tiledMap.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject)mapObject).getRectangle();
+
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rect.getX()+rect.getWidth()/2, rect.getY()+rect.getHeight()/2);
+
+            body = world.createBody(bodyDef);
+
+            polygonShape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+            fixtureDef.shape = polygonShape;
+            body.createFixture(fixtureDef);
+        }
+
+        // Create pipes bodies/fixtures
+        for(MapObject mapObject : tiledMap.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject)mapObject).getRectangle();
+
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rect.getX()+rect.getWidth()/2, rect.getY()+rect.getHeight()/2);
+
+            body = world.createBody(bodyDef);
+
+            polygonShape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+            fixtureDef.shape = polygonShape;
+            body.createFixture(fixtureDef);
+        }
+
+        // Create powers bodies/fixtures
+        for(MapObject mapObject : tiledMap.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject)mapObject).getRectangle();
+
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rect.getX()+rect.getWidth()/2, rect.getY()+rect.getHeight()/2);
+
+            body = world.createBody(bodyDef);
+
+            polygonShape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+            fixtureDef.shape = polygonShape;
+            body.createFixture(fixtureDef);
+        }
+
+        // Create bricks bodies/fixtures
+        for(MapObject mapObject : tiledMap.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject)mapObject).getRectangle();
+
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rect.getX()+rect.getWidth()/2, rect.getY()+rect.getHeight()/2);
+
+            body = world.createBody(bodyDef);
+
+            polygonShape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+            fixtureDef.shape = polygonShape;
+            body.createFixture(fixtureDef);
+        }
+
+        // Create coins bodies/fixtures
+        for(MapObject mapObject : tiledMap.getLayers().get(6).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject)mapObject).getRectangle();
+
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rect.getX()+rect.getWidth()/2, rect.getY()+rect.getHeight()/2);
+
+            body = world.createBody(bodyDef);
+
+            polygonShape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+            fixtureDef.shape = polygonShape;
+            body.createFixture(fixtureDef);
+        }
+
+        // Create secretBox bodies/fixtures
+        for(MapObject mapObject : tiledMap.getLayers().get(7).getObjects().getByType(RectangleMapObject.class)){
+            Rectangle rect = ((RectangleMapObject)mapObject).getRectangle();
+
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(rect.getX()+rect.getWidth()/2, rect.getY()+rect.getHeight()/2);
+
+            body = world.createBody(bodyDef);
+
+            polygonShape.setAsBox(rect.getWidth()/2, rect.getHeight()/2);
+            fixtureDef.shape = polygonShape;
+            body.createFixture(fixtureDef);
+        }
+
+
         //initially set the camera to be centered correctly at startup
         //viewport.getCamera().position.set(viewport.getWorldWidth()/2, viewport.getWorldHeight()/2, 0);
         //viewport.apply(true);
@@ -84,6 +188,9 @@ public class GameRenderer implements Disposable {
 
         // render game characters
         renderGamePlay2();
+
+        box2DDebugRenderer.render(world, viewport.getCamera().combined);
+
 
         // update camera (is not wrapped inside the alive conditions, because we
         // want to be able to control the camera even when the game is over)
